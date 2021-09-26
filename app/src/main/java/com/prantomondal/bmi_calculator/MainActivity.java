31bee8e31716786200e02cf850a59ddadb8abda6
PrantoMondal <kumar.pranto@northsouth.edu>
2021-09-26 13:54:43 +0600

    java file modified

--- app/src/main/java/com/prantomondal/bmi_calculator/MainActivity.java
+++ app/src/main/java/com/prantomondal/bmi_calculator/MainActivity.java
@@ -2,13 +2,25 @@ package com.prantomondal.bmi_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    android.widget.Button mcalculateBMI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        mcalculateBMI = findViewById(R.id.calculateBMI);
+        mcalculateBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,bmiactivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/prantomondal/bmi_calculator/bmiactivity.java
+++ app/src/main/java/com/prantomondal/bmi_calculator/bmiactivity.java
@@ -2,13 +2,27 @@ package com.prantomondal.bmi_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class bmiactivity extends AppCompatActivity {
 
+    android.widget.Button mrecalculateBMI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmiactivity);
+        getSupportActionBar().hide();
+        mrecalculateBMI = findViewById(R.id.recalculateBMI);
+        mrecalculateBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(bmiactivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
